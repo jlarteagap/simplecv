35f462937a382d78c8c483a3f6b4745c525f4855
Jorge <jorgelartearteagap@gmail.com>
2021-11-10 20:39:29 -0400

    Bulma CSS & Context and another configs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import React from 'react'
-import './assets/index.css'
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from 'react'
+import AppContext from './hoc/AppContext'
+import 'bulma/css/bulma.min.css'
+import Questions from './components/Questions'
+import questionsArray from './constants/questionsArray'
 
 function App() {
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+  const [questionAnswer, setQuestionAnswer] = useState({})
+
+  useEffect(() => {
+    setQuestions(questionsArray)
+    setQuestionAnswer(questionsArray[0])
+  }, [])
+
+  const handleChangeInput = e => {
+    setQuestionAnswer({
+      ...questionAnswer,
+      answer: e.target.value
+    })
+  }
+
+  const nextQuestion = e => {
+    e.preventDefault()
+    questions.map(question => {
+      if (question.resumeFieldId === questionAnswer.resumeFieldId) {
+        setAnswers([...answers, { ...question, answer: questionAnswer.answer }])
+      }
+    })
+
+    questions.map((qa, index) => {
+      if (index <= questions.length) {
+        if (qa.resumeFieldId === questionAnswer.resumeFieldId) {
+          setQuestionAnswer(questions[index + 1])
+        }
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <p>Hola</p>
-    </div>
+    <AppContext.Provider
+      value={{
+        state: {
+          questionAnswer,
+          questions,
+          answers
+        },
+        function: {
+          handleChangeInput: handleChangeInput,
+          nextQuestion: nextQuestion
+        }
+      }}
+    >
+      <div className="app container">
+        <h3 className="title has-text-centered">Resume Builder</h3>
+        <Questions />
+      </div>
+    </AppContext.Provider>
   )
 }
 
